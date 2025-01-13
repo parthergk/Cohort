45a770372112c7e884f31978a3f3367de3c3dcf2
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-13 14:21:56 +0530

    comp

--- week-7/db.js
+++ week-7/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: {type: String},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String,}
 })
 
 const taskSchema = new mongoose.Schema({
