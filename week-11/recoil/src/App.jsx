6a580c4a21a5dc7d586fb2e9f85e65e522b58f57
gaurav5xy <gauravkumar81464@gmail.com>
2025-01-20 10:58:22 +0530

    dark mode check

--- week-11/recoil/src/App.jsx
+++ week-11/recoil/src/App.jsx
@@ -1,6 +1,5 @@
 // App.js
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import "./App.css";
 import {
   counterRecoil,
   decSelector,
@@ -8,11 +7,14 @@ import {
 } from "./recoil/counterRecoil";
 
 const App = () => {
-    console.log("App");
-    
+  console.log("App");
+
   return (
-    <div className="container">
-      <div className="btn">
+    <div className=" flex flex-col gap-5 dark:bg-neutral-900 bg-white dark:text-white text-neutral-900 h-screen">
+      <button className=" border" onClick={()=>{
+        document.querySelector("html").classList.toggle("dark");
+      }}>Dark/Light</button>
+      <div className=" flex gap-5">
         <Increase />
         <Decrease />
       </div>
@@ -22,10 +24,11 @@ const App = () => {
 };
 
 function Increase() {
-    console.log("Increase btn");
+  console.log("Increase btn");
   const setCount = useSetRecoilState(counterRecoil);
   return (
     <button
+      className=" border px-1"
       onClick={() =>
         setCount((prev) => ({
           ...prev,
@@ -39,11 +42,12 @@ function Increase() {
 }
 
 function Decrease() {
-    console.log("Decrease btn");
-    
+  console.log("Decrease btn");
+
   const setCount = useSetRecoilState(counterRecoil);
   return (
     <button
+      className=" border px-1"
       onClick={() =>
         setCount((prev) => ({
           ...prev,
@@ -57,9 +61,9 @@ function Decrease() {
 }
 
 function Counter() {
-    console.log("Counter");
-    return (
-        <div>
+  console.log("Counter");
+  return (
+    <div>
       <IncCounter />
       <DecCounter />
     </div>
@@ -67,13 +71,13 @@ function Counter() {
 }
 
 function IncCounter() {
-    console.log("Increase");
-    const incCount = useRecoilValue(incSelector);
-    return <div>Increment Count: {incCount}</div>;
+  console.log("Increase");
+  const incCount = useRecoilValue(incSelector);
+  return <div>Increment Count: {incCount}</div>;
 }
 
 function DecCounter() {
-    console.log("Decrease");
+  console.log("Decrease");
   const decCount = useRecoilValue(decSelector);
   return <div>Decrement Count: {decCount}</div>;
 }
