726caedcbf460c1d2db2cf777fea520eebb64b7d
gaurav5xy <gauravkumar81464@gmail.com>
2025-02-10 11:34:06 +0530

    localstorage not on server side rendring

--- week-19/app/profile/page.tsx
+++ week-19/app/profile/page.tsx
@@ -1,19 +1,13 @@
-'use client'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-const Page = () => {
-    const [profile, setProfile] = useState();
-    
-    useEffect(()=>{
-        axios.get('http://localhost:3000/api/profile', {
+async function Page(){
+     const res = await axios.get('http://localhost:3000/api/profile', {
            headers:{
             authorization: localStorage.getItem('token')
            }
-        }).then((res)=>{
-            setProfile(res.data.url);
         })
-    },[])
+        const profile = res.data.url
   return (
     <div>URL:{profile}</div>
   )
