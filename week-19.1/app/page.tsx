ac21d050299dc3890bdecda95177d3515ba08d17
gaurav5xy <gauravkumar81464@gmail.com>
2025-02-13 11:09:07 +0530

    use session

--- week-19.1/app/page.tsx
+++ week-19.1/app/page.tsx
@@ -1,6 +1,19 @@
+"use client"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   return (
-    <div></div>
+    <SessionProvider>
+      <Landing/>
+    </SessionProvider>
   )
+}
+
+
+function Landing (){
+  const session = useSession();
+  return <div>
+    {session.status === "authenticated" && <button onClick={()=> signOut()}>Sign Out</button>}
+    {session.status === "unauthenticated" && <button onClick={()=> signIn()}>Sign In</button>}
+  </div>
 }
